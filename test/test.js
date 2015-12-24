92a1d6d2d82b08ba34d2cbc03898f72f60299c52
gwendoux <gwenael.doux@cncc.fr>
2015-12-24 14:58:17 +0100

    fixed test
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe("get links from pinboard rss feed", function() {
     it("must return links", function() {
         this.timeout(5000);
         return request(app).get('/api/feed')
-            .expect('Content-Type', 'text/plain')
+            .expect('Content-Type', 'application/json')
             .expect(200)
             .then(function(res) {
                 res.must.exist();
@@ -19,7 +19,7 @@ describe("get links from pinboard rss feed", function() {
 describe("get links from pinboard api", function() {
     it("must return links", function() {
         return request(app).get('/api/pin')
-            .expect('Content-Type', 'text/plain')
+            .expect('Content-Type', 'application/json')
             .expect(200)
             .then(function(res) {
                 res.must.exist();
@@ -31,6 +31,19 @@ describe("get links from pinboard api", function() {
 describe("get photos from instagram with a specific hashtag", function() {
     it("must return at least 1 result", function() {
         return request(app).get('/api/photos/coffeeoftheday')
+            .expect('Content-Type', 'text/plain')
+            .expect(200)
+            .then(function(res) {
+                res.must.exist();
+                res.text.must.not.be.empty();
+            });
+    });
+});
+
+
+describe("get photos liked by the specific user", function() {
+    it("must return at least 1 result", function() {
+        return request(app).get('/api/likes')
             .expect('Content-Type', 'text/plain')
             .expect(200)
             .then(function(res) {
