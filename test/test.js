e39bc61696ef162bdf8f1acc495368d086b3f912
gwendoux <gwenael.doux@cncc.fr>
2016-02-10 15:58:59 +0100

    modified url for tests
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- test/test.js
+++ test/test.js
@@ -2,10 +2,30 @@ require('must');
 var request = require('supertest-as-promised');
 var app = require('../app');
 
+describe("get 404", function() {
+    it("must return 404 for index page", function() {
+        return request(app).get('/')
+            .expect(404)
+            .then(function(res) {
+                res.must.exist();
+                res.text.must.contain('error 404');
+            });
+    });
+
+    it("must return 404 for random page", function() {
+        return request(app).get('/random/page')
+            .expect(404)
+            .then(function(res) {
+                res.must.exist();
+                res.text.must.contain('error 404');
+            });
+    });
+});
+
 describe("get links from Pinboard", function() {
     it("must return links from pinboard rss", function() {
         this.timeout(5000);
-        return request(app).get('/api/feed')
+        return request(app).get('/v1/links/feed')
             .expect(200)
             .then(function(res) {
                 res.must.be.json;
@@ -15,7 +35,7 @@ describe("get links from Pinboard", function() {
     });
 
     it("must return links from pinboard api", function() {
-        return request(app).get('/api/pin')
+        return request(app).get('/v1/links')
             .expect(200)
             .then(function(res) {
                 res.must.be.json;
@@ -27,7 +47,7 @@ describe("get links from Pinboard", function() {
 
 describe("get photos from Instagram API", function() {
     it("must return at least 1 result with a specific hashtag", function() {
-        return request(app).get('/api/photos/coffeeoftheday')
+        return request(app).get('/v1/photos/coffeeoftheday')
             .expect(200)
             .then(function(res) {
                 res.must.be.json;
@@ -36,9 +56,8 @@ describe("get photos from Instagram API", function() {
             });
     });
 
-
     it("must return at least 1 result liked by the specific user", function() {
-        return request(app).get('/api/likes')
+        return request(app).get('/v1/photos/likes')
             .expect(200)
             .then(function(res) {
                 res.must.be.json;
