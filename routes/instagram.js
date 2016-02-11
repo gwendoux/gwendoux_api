03975f8e83e8c4446bde527a70d3633b725e818d
gwendoux <gwenael.doux@cncc.fr>
2016-02-11 16:10:40 +0100

    refactored instagram module
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- routes/instagram.js
+++ routes/instagram.js
@@ -3,7 +3,6 @@ const config = require('../lib/config');
 
 const ig = require('instagram-node').instagram();
 const escape = require('escape-html');
-const moment = require('moment');
 // const logger = config.getLogger();
 
 ig.use({
@@ -18,30 +17,52 @@ function tag(req, res) {
         if (err) {
             throw err;
         }
-        var coffeeBeans = result.filter(function(photo) {
-            return photo.tags.indexOf(req.params.tag) > -1;
-        }).map(function(photo) {
-            return {
-                image_standard: photo.images.standard_resolution.url,
-                caption: escape(photo.caption.text),
-                date: moment(photo.created_time, 'X').fromNow()
-            };
-        });
+        var filterRequest = filterDataByTag(result, req.params.tag);
         res.setHeader('Content-Type', 'application/json');
-        res.jsonp(coffeeBeans);
+        res.jsonp(filterRequest);
     });
 }
 
 // get photos liked from a dedicated user
 function likes(req, res) {
-    ig.user_self_liked(function(err, data) {
+    ig.user_self_liked(function(err, result) {
         if (err) {
             throw err;
         }
+        var filterRequest = filterData(result);
         res.setHeader('Content-Type', 'application/json');
-        res.jsonp(data);
+        res.jsonp(filterRequest);
     });
 }
+
+function filterDataByTag(result, tag) {
+    return filterData(result.filter(function(photo) {
+        return photo.tags.indexOf(tag) > -1;
+    }));
+}
+
+function filterData(result) {
+    // only return images not videos
+    return result.filter(function(photo) {
+        return photo.type.indexOf('image') > -1;
+    }).map(function(photo) {
+        return {
+            image: {
+                standard: photo.images.standard_resolution.url,
+                thumbnail: photo.images.thumbnail.url,
+                caption: escape(photo.caption.text),
+                origlink: photo.link,
+                date: photo.created_time
+            },
+            user: {
+                username: photo.user.username,
+                profile_picture: photo.user.profile_picture
+            }
+        };
+    });
+}
+
+
 
 exports.tag = tag;
 exports.likes = likes;
