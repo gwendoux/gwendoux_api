a634aeefad2973a9a3b92c0770449eadbb2db44e
gwen <gwendoux@gmail.com>
2016-03-31 18:39:06 +0200

    Added redis caching for pinboard links
    expire every 2 minutes

--- routes/pinboard.js
+++ routes/pinboard.js
@@ -5,11 +5,14 @@ const config = require('../lib/config');
 const url = require('url');
 const escape = require('escape-html');
 
-// const logger = config.getLogger();
+const redis = require("redis");
+const client = redis.createClient();
+
+const logger = config.getLogger();
 const Pinboard_data = config.get('pinboard_feed_url');
 
 const Pinboard_API_Token = config.get('pinboard_API_TOKEN');
-const Pinboard_API_Endpoint = 'https://api.pinboard.in/v1/posts/recent?auth_token=' + Pinboard_API_Token + '&format=json';
+const Pinboard_API_Endpoint = 'https://api.pinboard.in/v1/posts/recent?auth_token=' + Pinboard_API_Token + '&format=json&count=12';
 
 function feed(req, res) {
     parser(Pinboard_data, function(err, json) {
@@ -31,13 +34,38 @@ function feed(req, res) {
 }
 
 function getData(req, res) {
-    request.get(Pinboard_API_Endpoint, function (err, response, data) {
-        if(err) {
-            throw err;
-        }
-        res.setHeader('Content-Type', 'application/json');
-        res.jsonp(data);
+    var content;
+    client.on('error', function (err) {
+        logger.debug('Error:', err);
     });
+    res.setHeader('Content-Type', 'application/json');
+    client.exists('items', function(err, reply) {
+        if (reply === 1) {
+            client.hget('items', 'items', function(err, data) {
+                if(err) {
+                    logger.debug(err);
+                }
+                content = JSON.parse(data).posts;
+                res.jsonp(content);
+            });
+        } else {
+            request.get(Pinboard_API_Endpoint, function (err, response, data) {
+                if(err) {
+                    throw err;
+                }
+                content = JSON.parse(data).posts;
+                client.hset('items', 'items', data);
+                // set expiration to 2 minutes
+                // pinboard allows 1 call/minute
+                client.expire('items', 120);
+                res.jsonp(content);
+
+            });
+        }
+    });
+
+
+
 }
 
 exports.feed = feed;
