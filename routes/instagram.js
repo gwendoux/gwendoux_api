e5527f7f207860878430a24f1b3230e27552b54e
gwen <gwendoux@gmail.com>
2016-04-01 12:35:54 +0200

    splitted code

--- routes/instagram.js
+++ routes/instagram.js
@@ -1,6 +1,6 @@
 
 const config = require('../lib/config');
-
+const utilities = require('../lib/photos-utilities');
 const ig = require('instagram-node').instagram();
 const escape = require('escape-html');
 const logger = config.getLogger();
@@ -36,7 +36,7 @@ function tag(req, res) {
                 if (err) {
                     throw err;
                 }
-                filterRequest = filterDataByTag(result, req.params.tag);
+                filterRequest = utilities.filterDataByTag(result, req.params.tag);
                 client.hset('photos', req.params.tag, JSON.stringify(filterRequest));
                 // set expiration to one day
                 // instagram photos does not need to update too often
@@ -54,40 +54,11 @@ function likes(req, res) {
         if (err) {
             throw err;
         }
-        var filterRequest = filterData(result);
+        var filterRequest = utilities.filterData(result);
         res.setHeader('Content-Type', 'application/json');
         res.jsonp(filterRequest);
     });
 }
-
-function filterDataByTag(result, tag) {
-    return filterData(result.filter(function(photo) {
-        return photo.tags.indexOf(tag) > -1;
-    }));
-}
-
-function filterData(result) {
-    // only return images not videos
-    return result.filter(function(photo) {
-        return photo.type.indexOf('image') > -1;
-    }).map(function(photo) {
-        return {
-            image: {
-                standard: photo.images.standard_resolution.url,
-                thumbnail: photo.images.thumbnail.url,
-                caption: photo.caption ? escape(photo.caption.text) : "",
-                origlink: photo.link,
-                date: photo.created_time
-            },
-            user: {
-                username: photo.user.username,
-                profile_picture: photo.user.profile_picture
-            }
-        };
-    });
-}
-
-
 
 exports.tag = tag;
 exports.likes = likes;
